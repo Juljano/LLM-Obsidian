4d3ad43c3202a707001f9df9c0a0537658b4c40b
Janosch <moellerjuljano@gmail.com>
2026-08-14 00:57:37 +0200

    feat(prompt): add Denise prompt for personal assistant functionality fix(main): update variable names for clarity and consistency chore(.gitignore): exclude Denise-prompt.md from version control

--- Scripts/main.py
+++ Scripts/main.py
@@ -5,12 +5,12 @@ from gemma4 import ask_gemma
 import uuid
 from sql_conversation import *
 
-db_memory = "/home/janosch/KI & ML-Projekte/LLM-Obsidian/conversation/memory.db"
-vault_path = Path("/home/janosch/AI-ML-Systems/AI & cML Systems")
+__db_memory = "/home/janosch/KI & ML-Projekte/LLM-Obsidian/conversation/memory.db"
+__vault_path = Path("/home/janosch/AI-ML-Systems/AI & ML Systems12")
 
 
 def get_notes():
-    return list(vault_path.rglob("*.md"))
+    return list(__vault_path.rglob("*.md"))
 
 def read_notes(path):
     return path.read_text(encoding="utf-8")
@@ -23,35 +23,31 @@ def extract_memory(content):
 
 if __name__ == "__main__":
     all_notes = get_notes()
-    context = ""
-    for note in all_notes:
-        relative_path = note.relative_to(vault_path)
+    if not all_notes:
+        print("Keine Notizen gefunden.")
+        context = ""
+    else:
+        context_parts = []
+        for note in all_notes:
+            relative_path = note.relative_to(__vault_path)
+            context_parts.append(f"Pfad- & Dateiname: {relative_path} ---\n")
+            context_parts.append(read_notes(note))
+        context = "".join(context_parts)
 
-        context += f"\n--- {relative_path} ---\n"
-        context += read_notes(note)
+while True:
+    question = input("Stelle eine Frage an Denise ('exit' zum Beenden): ")
 
-    while True:
+    if question.lower() == "exit":
+        break
 
-        question = input("Stelle eine Frage an Denise ('exit' zum Beenden): ")
+    answer = ask_gemma(question=question, context=context)
 
-        if question.lower() == "exit":
-            break
+    print(f"Denise: {answer}")
 
-        answer = ask_gemma(
-            question=question,
-            context=context
-        )
+    if "save" in answer and "true" in answer:
+        print("Die Erinnerung wird gespeichert in die Datenbank.")
+        memory = extract_memory(answer)
+        conversation_id = str(uuid.uuid4())
+        insert_memory(__db_memory, memory)
 
-        print(f"Denise: {answer}")
-
-        if "save" in answer and "true" in answer:
-            print("Die Antwort enthält 'true', daher wird sie in die Datenbank eingefügt.")
-            memory = extract_memory(answer)
-            conversation_id = str(uuid.uuid4())
-            insert_memory(db_memory, memory)
-
-
-
-
-    
 
